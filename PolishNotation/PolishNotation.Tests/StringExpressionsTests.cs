a7c1cb018a124f9bab3a52f2cad2ba1d41c301fa
Kostiantyn Kudriavtsev <kudriako@gmail.com>
2022-11-30 02:27:00 +0200

    PolishNotation. Create a testable solution.
    - Added processing single value.

--- PolishNotation/PolishNotation.Tests/StringExpressionsTests.cs
+++ PolishNotation/PolishNotation.Tests/StringExpressionsTests.cs
@@ -15,5 +15,16 @@ namespace PolishNotation.Tests
         {
             text.Evaluate().Should().BeApproximately(result, double.Epsilon);
         }
+
+        [Test]
+        [TestCase("0", 0d)]
+        [TestCase("42", 42d)]
+        [TestCase("0.9", 0.9d)]
+        [TestCase(".123", 0.123d)]
+        [TestCase("-78.1", -78.1d)]
+        public void Evaluate_WhenSingleValue_ReturnsParsedValue(string text, double result)
+        {
+            text.Evaluate().Should().BeApproximately(result, double.Epsilon);
+        }
     }
 }
--- PolishNotation/PolishNotation/StringExtensions.cs
+++ PolishNotation/PolishNotation/StringExtensions.cs
@@ -4,7 +4,33 @@
     {
         public static double Evaluate(this string text)
         {
-            return 0;
+            if (text == null || string.IsNullOrWhiteSpace(text))
+                return default(double);
+            var terms = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var stack = new Stack<string>(terms);
+            return BuildExpressionStack(stack).EvaluateExpressionStack();
+        }
+
+        private static Stack<Func<double>> BuildExpressionStack(Stack<string> terms)
+        {
+            var result = new Stack<Func<double>>();
+            var value = default(double);
+            foreach(var term in terms)
+            {
+                value = double.Parse(term);
+                result.Push(() => value);
+            }
+            return result;
+        }
+
+        private static double EvaluateExpressionStack(this Stack<Func<double>> expressions)
+        {
+            var result = 0.0;
+            foreach (var expression in expressions)
+            {
+                result = expression();
+            }
+            return result;
         }
     }
 }
